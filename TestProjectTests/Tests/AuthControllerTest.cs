0206445909ff99a5f1daa330b7cfc87729854b99
Guilherme Chiconato <guilherme.chiconato@gmail.com>
2019-07-07 23:53:25 -0300

    testes

---
+++ TestProjectTests/Tests/AuthControllerTest.cs
@@ -0,0 +1,60 @@
+﻿using FluentAssertions;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Test.ViewModels;
+using Xunit;
+
+namespace TestProjectTests.Tests
+{
+    public class AuthControllerTest
+    {
+        private readonly TestsContext _testsContext;
+
+        public AuthControllerTest()
+        {
+            _testsContext = new TestsContext();
+        }
+
+        [Fact]
+        public async Task Login_ReturnsOkResponse()
+        {
+            var login = new LoginViewModel()
+            {
+                Username = "teste",
+                Password = "1234"
+            };
+
+            var response = await _testsContext.Client.PostAsync("/api/login", DataToContent(login));
+            response.EnsureSuccessStatusCode();
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task Login_ReturnsBadRequest()
+        {
+            var login = new LoginViewModel()
+            {
+                Username = "login",
+                Password = "4567"
+            };
+
+            var response = await _testsContext.Client.PostAsync("/api/login", DataToContent(login));
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+
+        private ByteArrayContent DataToContent<T>(T data)
+        {
+            var myContent = JsonConvert.SerializeObject(data);
+
+            var buffer = System.Text.Encoding.UTF8.GetBytes(myContent);
+            var byteContent = new ByteArrayContent(buffer);
+
+            byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            return byteContent;
+        }
+    }
+}
---
+++ TestProjectTests/Tests/TestsContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System.Net.Http;
+using Test;
+
+namespace TestProjectTests.Tests
+{
+    internal class TestsContext
+    {
+        public HttpClient Client { get; set; }
+        private TestServer _server;
+
+        public TestsContext()
+        {
+            SetupClient();
+        }
+        private void SetupClient()
+        {
+            _server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            Client = _server.CreateClient();
+        }
+    }
+}
